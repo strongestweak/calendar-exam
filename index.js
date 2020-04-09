6754e016295b6e6650234a63311bf58a3ded5d72
Arnie Kevin <achipe@logixsoftwarelab.com>
2020-04-09 20:29:36 +0800

    change port to 80

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.use(cors())
 
 app.use("/api/schedule", require("./server/routes"))
 
-app.listen(process.env.PORT || '5000', () => {
-    console.log(`Server is running on port: ${process.env.PORT || '5000'}`)
+app.listen(process.env.PORT || '80', () => {
+    console.log(`Server is running on port: ${process.env.PORT || '80'}`)
 })
