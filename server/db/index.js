31c6e5885fec7c042d328c88e84f6b5c1c0b649f
Arnie Kevin <achipe@logixsoftwarelab.com>
2020-04-12 01:39:30 +0800

    update password

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ const mysql = require("mysql")
 
 const conn = mysql.createPool({
     connectionLimit: 10,
-    password: '',
+    password: 'password.1',
     user: 'root',
     database: 'DB_Schedule',
     host: 'localhost',
